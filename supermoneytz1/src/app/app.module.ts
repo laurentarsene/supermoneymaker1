70e027d18c7ba23bcf0205dc36b8fa2cef03a9a9
laurentarsene <arsene.laurent@student.uclouvain.be>
2023-03-11 21:01:04 +0100

    login component added

--- supermoneytz1/src/app/app.module.ts
+++ supermoneytz1/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import {MatInputModule} from '@angular/material/input'; 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
