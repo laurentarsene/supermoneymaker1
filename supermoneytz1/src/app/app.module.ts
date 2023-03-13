be1b25d9d19db607291253bdef5ab3d403d88084
laurentarsene <arsene.laurent@student.uclouvain.be>
2023-03-13 22:05:09 +0100

    :feat:j'ai commencé les login avec firebase le sang

--- supermoneytz1/src/app/app.module.ts
+++ supermoneytz1/src/app/app.module.ts
@@ -6,6 +6,52 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsl93dmcxaADHObuKTzLHsFJ8lzU7Xm9w",
+  authDomain: "supermoneytz.firebaseapp.com",
+  projectId: "supermoneytz",
+  storageBucket: "supermoneytz.appspot.com",
+  messagingSenderId: "802699938263",
+  appId: "1:802699938263:web:5d00c9f36a2f7f073aa392",
+  measurementId: "G-1GTJZYNJGM"
+};
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
 
 
 @NgModule({
@@ -16,7 +62,8 @@ import {MatInputModule} from '@angular/material/input';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
